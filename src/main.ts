e66de55d33206cba655117350898d5dc6940f92d
Andrew G. Knackstedt <knackstedt@outlook.com>
2025-03-04 10:54:59 -0500

    feat: automatic reconnection

--- src/main.ts
+++ src/main.ts
@@ -39,11 +39,20 @@ export class SurrealDBStore extends Store {
 
     private db: Surreal;
     private tableName: string;
+    private lastConnectionAttempt = 0;
+    private hasConnected = false;
+    private isConnected = false;
 
     constructor(private readonly options: SurrealDBStoreOptions) {
         super();
 
         this.db = options.surreal ?? new Surreal();
+
+        // Re-connect
+        this.db.emitter.subscribe("disconnected", () => {
+            this.isConnected = false;
+            this._reconnect();
+        });
 
         // Preventative for SQLi if the developer hasn't hardcoded this.
         if (options.tableName && /^[a-zA-Z0-9]+$/.test(options.tableName))
@@ -56,23 +65,83 @@ export class SurrealDBStore extends Store {
         });
     }
 
+    /**
+     * Perform the initial connection to the database. This also sets the scope of our connection.
+     */
     private async _connect() {
         await this.db.connect(this.options.url, this.options.connectionOpts);
         await this.db.signin(this.options.signinOpts);
-        if (this.options.useOpts)
-            await this.db.use(this.options.useOpts)
+
+        if (this.options.useOpts) {
+            await this.db.use(this.options.useOpts);
+        }
+    }
+
+    /**
+     * Reconnect to the database if our connection drops. This uses a connection
+     * throttling technique to prevent connection storming.
+     */
+    private async _reconnect() {
+        if (this.isConnected) return;
+
+        // If this last tried to connect under 5 seconds ago, abort.
+        // This prevents overloading the network with connection attempts.
+        if (Date.now() - this.lastConnectionAttempt < 5000) {
+            if (this.hasConnected) {
+                console.error("The connection to SurrealDB appears to have dropped.");
+            }
+            else {
+                console.error("Cannot reconnect to SurrealDB.");
+            }
+            return;
+        }
+
+        this.lastConnectionAttempt = Date.now();
+        await this.db.connect(this.options.url, this.options.connectionOpts)
+            .then(() => {
+                this.isConnected = true;
+            });
+    }
+
+    /**
+     * Check the connection state and attempt to reconnect before continuing
+     * This ensures that sessions shouldn't observe disruptions in edge cases
+     * where the connection gets lost and we can't connect immediately.
+     *
+     * If there's 50 minutes between the drop and a new connection, the user won't
+     * get an error screen and need to refresh their page.
+     */
+    private async _checkConnectionAndReconnect() {
+        if (!this.isConnected) {
+            if (this.hasConnected) {
+                // The connection has dropped once and we just need to reconnect.
+                await this._reconnect();
+            }
+            else {
+                // In the case where the initial connection
+                // fails and is resolved after startup.
+                await this._connect();
+            }
+        }
     }
 
 	get(sessionId: string, cb: Function) {
-        this.db.select(new RecordId(this.tableName, sessionId))
-            .then((res) => cb(null, res))
-            .catch(err => cb(err))
+        this._checkConnectionAndReconnect()
+        .then(() => {
+            this.db.select(new RecordId(this.tableName, sessionId))
+                .then((res) => cb(null, res))
+                .catch(err => cb(err))
+        })
+        .catch(err => cb(err))
     }
 
     set(sessionId: string, session, cb: Function) {
-        // should this be a record (r)?
-        this.db.upsert(new RecordId(this.tableName, sessionId), session)
-            .then(() => cb(null))
+        this._checkConnectionAndReconnect()
+            .then(() => {
+                this.db.upsert(new RecordId(this.tableName, sessionId), session)
+                    .then((res) => cb(null, res))
+                    .catch(err => cb(err));
+            })
             .catch(err => cb(err))
     }
 
